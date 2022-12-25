60901ee2ef7d618eef145115f4ad8888eb7ff9b0
Shyam <butanishyam111@gmail.com>
2022-12-25 16:50:53 +0530

    Home page completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechGURU</h1>
+        <p>All Rights Reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/shyam-butani/" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="https://github.com/butani111" target={"blank"}>
+            GitHub
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
