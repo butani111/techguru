d0aaf29beadef4c0be8059ea8728aeda079bd27c
Shyam <butanishyam111@gmail.com>
2022-12-25 17:55:05 +0530

    Services page completed
    Got familier with react-responsive-carousel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import Services from "./components/Services";
 
 import "./styles/App.scss";
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
